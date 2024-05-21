e5e7c69140538deeb5d6ae2d056a5d40f443b739
Erik Shafer <erik.shafer@gmail.com>
2024-05-20 23:50:52 -0500

    appended to stream, read from stream, console print

--- StudentEnrollmentConsoleApp/Program.cs
+++ StudentEnrollmentConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using EventStore.Client;
+﻿using System.Text;
+using System.Text.Json;
+using EventStore.Client;
 using StudentEnrollmentConsoleApp;
 using StudentEnrollmentConsoleApp.Events;
 
@@ -13,6 +15,8 @@ var studentCreated = new StudentCreated
     Email = "erik.shafer@eventstore.com",
     DateOfBirth = new DateTime(1987, 1, 1)
 };
+const string eventType = nameof(studentCreated); //"StudentCreated";
+const string streamName = "some-stream";
 
 var inMemoryDb = new StudentDatabase();
 inMemoryDb.Append(studentCreated);
@@ -20,3 +24,35 @@ inMemoryDb.Append(studentCreated);
 const string connectionString = "esdb://admin:changeit@localhost:2113?tls=false&tlsVerifyCert=false";
 var settings = EventStoreClientSettings.Create(connectionString);
 var client = new EventStoreClient(settings);
+
+var eventData = new EventData(
+    Uuid.NewUuid(),
+    eventType,
+    JsonSerializer.SerializeToUtf8Bytes(studentCreated)
+);
+
+await client.AppendToStreamAsync(
+    streamName,
+    StreamState.Any,
+    new[] { eventData },
+    cancellationToken: default
+);
+
+var result = client.ReadStreamAsync(
+    Direction.Forwards,
+    streamName,
+    StreamPosition.Start,
+    cancellationToken: default
+);
+
+var events = await result.ToListAsync();
+
+foreach (var @event in events)
+{
+    Console.WriteLine($"EventType: {@event.Event.EventId}");
+    Console.WriteLine($"EventStreamId: {@event.Event.EventStreamId}");
+    Console.WriteLine($"EventType: {@event.Event.EventType}");
+    var data = Encoding.UTF8.GetString(@event.Event.Data.ToArray());
+    Console.WriteLine($"Data: {data}");
+    Console.WriteLine("-----");
+}
