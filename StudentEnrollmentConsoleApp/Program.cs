86c04c83406f316f411fd33ab7fd690bbe35eef1
Erik Shafer <erik.shafer@gmail.com>
2024-05-21 14:47:14 -0500

    removed references to inmem db, refactoring

--- StudentEnrollmentConsoleApp/Program.cs
+++ StudentEnrollmentConsoleApp/Program.cs
@@ -1,125 +1,82 @@
 ﻿using System.Text;
 using System.Text.Json;
 using EventStore.Client;
-using StudentEnrollmentConsoleApp;
 using StudentEnrollmentConsoleApp.Events;
 
 var id = Guid.Parse("a662d446-4920-415e-8c2a-0dd4a6c58908");
 var streamId = $"student-{id}";
 
-var created = new StudentCreated
-{
-    StudentId = streamId,
-    FullName = "Erik Shafer",
-    Email = "erik.shafer@eventstore.com",
-    DateOfBirth = new DateTime(1987, 1, 1)
-};
-var createdEventData = new EventData(
+var created = new EventData(
     Uuid.NewUuid(),
     "StudentCreated",
-    JsonSerializer.SerializeToUtf8Bytes(created)
+    JsonSerializer.SerializeToUtf8Bytes(new StudentCreated
+    {
+        StudentId = streamId,
+        FullName = "Erik Shafer",
+        Email = "erik.shafer@eventstore.com",
+        DateOfBirth = new DateTime(1987, 1, 1)
+    })
 );
 
-var enrolled = new StudentEnrolled
-{
-    StudentId = streamId,
-    CourseName = "From Zero to Hero: REST APis in .NET"
-};
-var enrolledEventData = new EventData(
+var enrolled = new EventData(
     Uuid.NewUuid(),
     "StudentEnrolled",
-    JsonSerializer.SerializeToUtf8Bytes(enrolled)
+    JsonSerializer.SerializeToUtf8Bytes(new StudentEnrolled
+    {
+        StudentId = streamId,
+        CourseName = "From Zero to Hero: REST APis in .NET"
+    })
 );
 
-var updated = new StudentUpdated
-{
-    StudentId = streamId,
-    FullName = "Erik Shafer",
-    Email = "erik.shafer.new.email@eventstore.com",
-};
-var updatedEventData = new EventData(
+var updated = new EventData(
     Uuid.NewUuid(),
     "StudentUpdated", 
-    JsonSerializer.SerializeToUtf8Bytes(updated)
+    JsonSerializer.SerializeToUtf8Bytes(new StudentUpdated
+    {
+        StudentId = streamId,
+        FullName = "Erik Shafer",
+        Email = "erik.shafer.new.email@eventstore.com",
+    })
 );
 
-// our in-memory database
-var inMemoryDb = new StudentDatabase();
-inMemoryDb.Append(created);
-inMemoryDb.Append(enrolled);
-inMemoryDb.Append(updated);
-
-// our EventStoreDB (ESDB)
+// Our EventStoreDB (ESDB)
 const string connectionString = "esdb://admin:changeit@localhost:2113?tls=false&tlsVerifyCert=false";
 var settings = EventStoreClientSettings.Create(connectionString);
 var client = new EventStoreClient(settings);
 
-// append to ESDB
+// Append to ESDB
 await client.AppendToStreamAsync(
     streamId,
     StreamState.Any,
-    new[] { createdEventData, enrolledEventData, updatedEventData },
+    new[] { created, enrolled, updated },
     cancellationToken: default
 );
 
-// read from ESDB
-var result = client.ReadStreamAsync(
+// Read from ESDB
+var readStreamResult = client.ReadStreamAsync(
     Direction.Forwards,
     streamId,
     StreamPosition.Start,
     cancellationToken: default
 );
+var eventStream = await readStreamResult.ToListAsync();
 
-var events = await result.ToListAsync();
-
-foreach (var @event in events)
+// Write out the events from the stream
+foreach (var @event in eventStream)
 {
     Console.WriteLine($"EventId: {@event.Event.EventId}");
     Console.WriteLine($"EventStreamId: {@event.Event.EventStreamId}");
     Console.WriteLine($"EventType: {@event.Event.EventType}");
-    // var data = JsonSerializer.Deserialize<StudentCreated>(@event.Event.Data.ToArray()); // TODO
-    var data = Encoding.UTF8.GetString(@event.Event.Data.ToArray());
-    Console.WriteLine($"Data: {data}");
+    Console.WriteLine($"Data: {Encoding.UTF8.GetString(@event.Event.Data.ToArray())}");
     Console.WriteLine("---");
 }
 
-var studentFromInMemDb = inMemoryDb.GetStudent(id);
-Console.Write(studentFromInMemDb);
-Console.WriteLine("");
+// Write out all the courses the student enrolled in
+var enrolledCourses = eventStream
+    .Where(re => re.Event.EventType == "StudentEnrolled")
+    .Select(re => JsonSerializer.Deserialize<StudentEnrolled>(re.Event.Data.ToArray()))
+    .Select(se => se!.CourseName)
+    .ToArray();
 
-var studentFromEsdb = new Student();
-foreach (var @event in events)
-{
-    var eventType = @event.Event.EventType;
-    var dataUtf8 = @event.Event.Data.ToArray();
-    switch (eventType)
-    {
-        case "StudentCreated":
-        {
-            var deserialized = JsonSerializer.Deserialize<StudentCreated>(dataUtf8);
-            studentFromEsdb.Apply(deserialized!);
-            break;
-        }
-        case "StudentEnrolled":
-        {
-            var deserialized = JsonSerializer.Deserialize<StudentEnrolled>(dataUtf8);
-            studentFromEsdb.Apply(deserialized!);
-            break;
-        }
-        case "StudentUpdated":
-        {
-            var deserialized = JsonSerializer.Deserialize<StudentUpdated>(dataUtf8);
-            studentFromEsdb.Apply(deserialized!);
-            break;
-        }
-        case "StudentUnEnrolled":
-        {
-            var deserialized = JsonSerializer.Deserialize<StudentUnEnrolled>(dataUtf8);
-            studentFromEsdb.Apply(deserialized!);
-            break;
-        }
-    }
-}
-
-Console.Write(studentFromEsdb);
+Console.WriteLine($"Courses enrolled in: {enrolledCourses}");
 Console.WriteLine("");
