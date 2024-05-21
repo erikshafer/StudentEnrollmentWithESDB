271374b074a69061ecf3c8fcfdb5719d655a2070
Erik Shafer <erik.shafer@gmail.com>
2024-05-20 21:34:53 -0500

    instantiate the first event

--- StudentEnrollmentConsoleApp/Program.cs
+++ StudentEnrollmentConsoleApp/Program.cs
@@ -1,1 +1,14 @@
-﻿{}
+﻿using StudentEnrollmentConsoleApp.Events;
+
+var id = Guid.Parse("a662d446-4920-415e-8c2a-0dd4a6c58908");
+var studentId = $"student-{id}";
+var now = DateTime.Now;
+
+var studentCreated = new StudentCreated
+{
+    CreatedAtUtc = now.ToUniversalTime(),
+    StudentId = studentId,
+    FullName = "Erik Shafer",
+    Email = "erik.shafer@eventstore.com",
+    DateOfBirth = new DateTime(1987, 1, 1)
+};
