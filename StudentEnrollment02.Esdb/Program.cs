b59d22ea83e7df7fc9eaa5c28b9f2c61643a5d87
Erik Shafer <erik.shafer@gmail.com>
2024-05-22 15:33:33 -0500

    expanded comments in proj 02 to provide further clarify

--- StudentEnrollment02.Esdb/Program.cs
+++ StudentEnrollment02.Esdb/Program.cs
@@ -3,15 +3,12 @@ using System.Text.Json;
 using EventStore.Client;
 using StudentEnrollment02.Esdb.Events;
 
-// Register events to a singleton for ease-of-reference
-EventTypeMapper.Instance.ToName(typeof(StudentCreated));
-EventTypeMapper.Instance.ToName(typeof(StudentEnrolled));
-EventTypeMapper.Instance.ToName(typeof(StudentWithdrawn));
-EventTypeMapper.Instance.ToName(typeof(StudentUpdated));
+// Validate GUID shape, then stringify. Same exact value as the in-memory version (01).
+var streamId = Guid.Parse("a662d446-4920-415e-8c2a-0dd4a6c58908").ToString();
 
-var id = Guid.Parse("a662d446-4920-415e-8c2a-0dd4a6c58908");
-var streamId = $"student-{id}";
-
+// Create the student events,
+// serialize them to JSON,
+// and then place them inside the EventData object from the ESDB client.
 var created = new EventData(
     Uuid.NewUuid(),
     "StudentCreated",
@@ -48,12 +45,15 @@ var emailChanged = new EventData(
     })
 );
 
-// Our EventStoreDB (ESDB)
+// Our EventStoreDB (ESDB) connection details
 const string connectionString = "esdb://admin:changeit@localhost:2113?tls=false&tlsVerifyCert=false";
 var settings = EventStoreClientSettings.Create(connectionString);
 var client = new EventStoreClient(settings);
 
-// Append to ESDB
+// Append the events to ESDB.
+// The StreamState is set to Any, but it could also be
+// NoStream to ensure nothing exists (otherwise an exception will throw), or
+// StreamExists to ensure it isn't new (AKA being created right through this action).
 await client.AppendToStreamAsync(
     streamId,
     StreamState.Any,
@@ -61,7 +61,8 @@ await client.AppendToStreamAsync(
     cancellationToken: default
 );
 
-// Read from ESDB
+// Read from ESDB. Specifically from the start of the stream
+// we've been working with for our example.
 var readStreamResult = client.ReadStreamAsync(
     Direction.Forwards,
     streamId,
@@ -70,7 +71,9 @@ var readStreamResult = client.ReadStreamAsync(
 );
 var eventStream = await readStreamResult.ToListAsync();
 
-// Write out the events from the stream
+// Write out the events from the stream.
+// We're printing out to Console exactly as-is from the ESDB client.
+// No mapping to a C# object, like Student, or anything like that. Yet.
 Console.WriteLine("Events from selected stream: ");
 foreach (var resolved in eventStream)
 {
@@ -81,7 +84,9 @@ foreach (var resolved in eventStream)
     Console.WriteLine("");
 }
 
-// Write out all the courses the student enrolled in
+// An example of searching through the events for a specific 
+// event type and then for a specific value in that type of event.
+// AKA, writing out all the courses our student enrolled in.
 var enrolledCourses = eventStream
     .Where(re => re.Event.EventType == "StudentEnrolled")
     .Select(re => JsonSerializer.Deserialize<StudentEnrolled>(re.Event.Data.ToArray()))
@@ -89,20 +94,4 @@ var enrolledCourses = eventStream
     .ToList();
 Console.WriteLine("Courses enrolled in: ");
 enrolledCourses.ForEach(ec => Console.WriteLine($"\t- {ec}"));
-Console.WriteLine("");
-
-// Write out using the mapper
-Console.WriteLine("Deserialized events:");
-foreach (var resolved in eventStream)
-{
-    var eventType = EventTypeMapper.Instance.ToType(resolved.Event.EventType);
-
-    if (eventType == null)
-        break;
-
-    var deserializedEvent = JsonSerializer.Deserialize(Encoding.UTF8.GetString(resolved.Event.Data.Span), eventType);
-    
-    Console.WriteLine($"\t{deserializedEvent}");
-}
-
 Console.WriteLine("");
